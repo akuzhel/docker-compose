ed0cd15b9da6f016689397fa429fc6b919edabd1
Arkadij Kuzhel <arkuzhel@gmail.com>
2016-12-14 14:08:34 +0200

    Keep extra info in .env file

--- dns/write-hosts.sh
+++ dns/write-hosts.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
 
-NETWORK_DATA="network.json"
-HOST='dev.gazelle.nl'
-HOST_SUFFIX='-docker-auto'
-BRIDGE_NETWORK_ID=`docker network ls|grep 'pon-tier'|awk ' {print $1}'`
+# Read variables
+source ../.env
 
-docker network inspect ${BRIDGE_NETWORK_ID} > $NETWORK_DATA
-#echo php write-hosts.php -i ${NETWORK_DATA} -h ${HOST} -s ${HOST_SUFFIX} ####
-#exit  ##########
-CONFIG_FILE=`php write-hosts.php -i $NETWORK_DATA -h $HOST -s $HOST_SUFFIX |xargs`
+# Dump network data
+BRIDGE_NETWORK_NAME=`docker network ls|grep "${NETWORK_NAME}"|awk ' {print $2}'`
+docker network inspect ${BRIDGE_NETWORK_NAME} > ${NETWORK_INFO_DUMP}
+echo 'Dumped network data for "'${BRIDGE_NETWORK_NAME}'"'
 
+# Prepare dnsmasq configuration file in PHP script
+CONFIG_FILE=`php write-hosts.php -i ${NETWORK_INFO_DUMP} -h $HOST -s ${CONF_FILE_SUFFIX} |xargs`
+
+# Put dnsmasq configuration file inplace and restart service
 if [[ -n ${CONFIG_FILE} ]]; then
     SOURCE=`pwd`/${CONFIG_FILE}
     echo Generated file '"'${SOURCE}'"';
     TARGET=/etc/dnsmasq.d/"$CONFIG_FILE"
 
     if [[ -f ${TARGET} ]]; then
-        sudo rm -f $TARGET
+        sudo rm -f ${TARGET}
         echo Removed previous config '"'${TARGET}'"'
     fi
 
     sudo cp  ${SOURCE} ${TARGET} && \
     sudo service dnsmasq restart
+    echo DNS service:
+    sudo service dnsmasq status
+
+    rm ${NETWORK_INFO_DUMP}
 fi
 
